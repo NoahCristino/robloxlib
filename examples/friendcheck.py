7e3da0f0ba892b711e37d5274e76476df1e3e6f4
Noah Cristino <noahcristino@yahoo.com>
2016-11-19 19:56:01 -0500

    Added an example

---
+++ examples/friendcheck.py
@@ -0,0 +1,6 @@
+import robloxlib
+friends = robloxlib.checkFriends(261, 504316) #shedletsky = 261, brighteyes = 504316
+if friends == True:
+	print "Shedletsky and BrightEyes are friends!"
+else:
+	print "Shedletsky and BrightEyes are not friends!"
