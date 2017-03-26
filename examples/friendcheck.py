439b54c673c9e508e37cb74a1f7ba7a1f6e21196
Noah Cristino <noahcristino@yahoo.com>
2017-03-26 09:12:33 -0400

    Delete friendcheck.py

--- examples/friendcheck.py
+++
@@ -1,6 +0,0 @@
-import robloxlib
-friends = robloxlib.checkFriends(261, 504316) #shedletsky = 261, brighteyes = 504316
-if friends == True:
-	print "Shedletsky and BrightEyes are friends!"
-else:
-	print "Shedletsky and BrightEyes are not friends!"
