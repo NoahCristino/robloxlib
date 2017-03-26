b30d8aac51bf45461c3a14f96d4e31392a7ac115
Noah Cristino <noahcristino@yahoo.com>
2017-03-26 09:10:47 -0400

    Changed print statements to return

--- robloxlib.py
+++ robloxlib.py
@@ -52,22 +52,22 @@ def getPrimaryGroupInfo(mode, username):
     if mode == "GroupId":
         data = r.json()
         username1 = str(username)
-        print(data[username1]['GroupId'])
+        return data[username1]['GroupId']
     if mode == "GroupName":
         data = r.json()
         username1 = str(username)
-        print(data[username1]['GroupName'])
+        return data[username1]['GroupName']
     if mode == "GroupRole":
         data = r.json()
         username1 = str(username)
-        print(data[username1]['RoleSetName'])
+        return data[username1]['RoleSetName']
     else:
         print("An error has occured, please check spelling.")
 def getPackageIds(packageid):
     try:
         r = requests.get("https://web.roblox.com/Game/GetAssetIdsForPackageId?packageId="+str(packageid))
         a = r.text
-        print(a)
+        return a
     except Exception as e:
         print("")
         print("A error has occured, please see below.")
