d0575246ce265cd8b0df68eac68d1648618a39de
dogesum <emmettjbrewer@icloud.com>
2017-03-25 23:03:54 -0500

    Create setup.py

---
+++ testing/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "robloxlib",
+    version = "0.0.1",
+    author = "Noah Cristino",
+    description = ("A simple library for getting info from ROBLOX's APIs."),
+    license = "MIT",
+    keywords = "roblox robloxapi requests",
+    packages=['robloxlib'],
+    url = "https://github.com/NoahCristino/robloxlib",
+    long_description= (""),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
